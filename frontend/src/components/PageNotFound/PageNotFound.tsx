04f7dea5dfd1df4ad77ee2cb1cef426eca815ed9
Björn <41452212+bjoern621@users.noreply.github.com>
2025-04-23 21:36:05 +0200

    react router example

---
+++ frontend/src/components/PageNotFound/PageNotFound.tsx
@@ -0,0 +1,12 @@
+import "./PageNotFound.scss";
+
+export function PageNotFound() {
+    return (
+        <div className="container">
+            <h1 className="heading">404 - Page Not Found</h1>
+            <p className="paragraph">
+                Sorry, the page you are looking for does not exist.
+            </p>
+        </div>
+    );
+}
--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -2,9 +2,17 @@ import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import "./index.scss";
 import App from "./components/App/App.tsx";
+import { BrowserRouter, Route, Routes } from "react-router";
+import { PageNotFound } from "./components/PageNotFound/PageNotFound.tsx";
 
 createRoot(document.getElementById("root")!).render(
     <StrictMode>
-        <App />
+        <BrowserRouter>
+            <Routes>
+                <Route path="/" element={<PageNotFound />} />
+                <Route path="app" element={<App />} />
+                <Route path="*" element={<PageNotFound />} />
+            </Routes>
+        </BrowserRouter>
     </StrictMode>
 );
