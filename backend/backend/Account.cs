9cbdc47273ff4fdb12c9852c187fbc0f54eb4236
infwuq321 <wuq321@haw-hamburg.de>
2025-05-01 19:23:18 +0200

    "Initial Setup for Account Creation"

---
+++ backend/backend/Account.cs
@@ -0,0 +1,33 @@
+﻿namespace backend;
+
+public class Account
+{
+    private string _displayName;
+    private string _password;
+    
+    // vielleicht auch statt displayname die email angeben können..
+    public Account(string displayName, string password)
+    {
+        if (displayName == null || password == null)
+        {
+            throw new ArgumentException("Please provide both displayName and password");
+        }
+        _displayName = displayName;
+        _password = encryptPassword(password);
+    }
+
+    public string getName() {
+        return _displayName;
+    }
+
+    public string getPassword() {
+        return _password;
+    }
+    /**
+     * Hash und Salt des Passwortes.
+     */
+    private string encryptPassword(string password)
+    {
+        throw new System.NotImplementedException();
+    }
+}
