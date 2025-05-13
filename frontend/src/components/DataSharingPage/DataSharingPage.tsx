00378f0e39ee83dfb0b6fa18f48b5d100614fd77
Tim <timrasegg@gmail.com>
2025-05-13 16:57:23 +0200

    better margins on header row and example of longer filenames

--- frontend/src/components/DataSharingPage/DataSharingPage.tsx
+++ frontend/src/components/DataSharingPage/DataSharingPage.tsx
@@ -37,6 +37,13 @@ const mockData: File[] = [
         size: 30,
         time: new Date(),
     },
+    {
+        name: 'super_log_filename_12345678901234567890_abcdefghijklmnopqrstuvwxyzilename_12345678901234567890_abcdefghijklmnopqrstuvwxyz.txt',
+        direction: FileDirection.UP,
+        progress: 0.7,
+        size: 30,
+        time: new Date(),
+    },
 ];
 
 export function DataSharingPage() {
