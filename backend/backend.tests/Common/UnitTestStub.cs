f8fbb0c31e090cbbb0d2fddbfba0615de029202e
infwuq321 <wuq321@haw-hamburg.de>
2025-05-23 18:51:58 +0200

    grundsätzliche Ordnerstruktur + NUNIT

---
+++ backend/backend.tests/Common/UnitTestStub.cs
@@ -0,0 +1,16 @@
+﻿namespace backend.tests.Common;
+
+
+public class Tests
+{
+    [SetUp]
+    public void Setup()
+    {
+    }
+
+    [Test]
+    public void Test1()
+    {
+        Assert.Pass();
+    }
+}
