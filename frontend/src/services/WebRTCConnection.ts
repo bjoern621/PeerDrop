38e0f93756aac3fb8cc05b1e7db50d7575729657
Björn <41452212+bjoern621@users.noreply.github.com>
2025-06-02 16:41:40 +0200

    Nicht benötigt?

--- frontend/src/services/WebRTCConnection.ts
+++ frontend/src/services/WebRTCConnection.ts
@@ -46,7 +46,7 @@ export class WebRTCConnection {
         this.handleSDPPackage();
         this.handleRemoteICECandidates();
 
-        this.testMethodDataChannelInitializier();
+        // this.testMethodDataChannelInitializier(); // <- Warum wird diese Methode hier aufgerufen? Sollte sie nicht ausschließlich in PeerConnectionManager aufgerufen werden?
     }
 
     public testMethodDataChannelInitializier() {
