5d7c441aae03f8bbe34652bc5c558ea3e7100d7d
Tim <wvz241@haw-hamburg.de>
2025-06-05 00:00:19 +0200

    documentation

--- frontend/src/components/dtos/LoginResponse.ts
+++ frontend/src/components/dtos/LoginResponse.ts
@@ -1,3 +1,8 @@
+/**
+ * Response from backend when User logs in or fetches their user name.
+ * 
+ * message: Successful login message or the display name of the user.
+ */
 export interface LoginResponse {
     message: string;
 }
