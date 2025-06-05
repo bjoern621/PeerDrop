afc414be98bc83ea26550f05b9b58cc97e190708
NovaCaller <dev@novacaller.com>
2025-06-05 18:50:20 +0200

    allow common namespace in addition to api, stricter matching via regex

--- backend/backend.tests/Common/DependOnOtherComponentOnlyThroughApiNamespaceCustomCondition.cs
+++ backend/backend.tests/Common/DependOnOtherComponentOnlyThroughApiNamespaceCustomCondition.cs
@@ -1,3 +1,4 @@
+using System.Text.RegularExpressions;
 using ArchUnitNET.Domain;
 using ArchUnitNET.Fluent.Conditions;
 
@@ -71,12 +72,15 @@ namespace backend.tests.Common
                     if (sourceComponentName != targetComponentName)
                     {
                         string targetFullNamespace = targetType.Namespace.FullName;
-                        bool isApiDependency = targetFullNamespace.Contains("Api");
+                        Regex apiRegex = new Regex(@"\.Api(?:$|\.)");
+                        bool isApiDependency = apiRegex.IsMatch(targetFullNamespace);
+                        Regex commonRegex = new Regex(@"\.Common(?:$|\.)");
+                        bool isCommonDependency = commonRegex.IsMatch(targetFullNamespace);
 
-                        if (!isApiDependency)
+                        if (!isApiDependency && !isCommonDependency)
                         {
                             overallPassForSourceType = false;
-                            failureDetails.Add($"{failedTestCount + 1} - depends on {targetType.FullName} (in component {targetComponentName}) which is not in an 'Api' namespace of that component");
+                            failureDetails.Add($"{failedTestCount + 1} - depends on {targetType.FullName} (in component {targetComponentName}) which is not in an 'Api' or 'Common' namespace of that component");
 
                             failedTestCount++;
                         }
