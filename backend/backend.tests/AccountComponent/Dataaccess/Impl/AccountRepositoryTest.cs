f184e836b400d2805cf8198accd926c8a1f58dc7
NovaCaller <dev@novacaller.com>
2025-06-06 15:15:18 +0200

    sanity check

--- backend/backend.tests/AccountComponent/Dataaccess/Impl/AccountRepositoryTest.cs
+++ backend/backend.tests/AccountComponent/Dataaccess/Impl/AccountRepositoryTest.cs
@@ -55,6 +55,8 @@ public class AccountRepositoryTest
         var account = new Account("testuser", "testpassword");
         var id = _accountRepository.SaveAsync(account).Result;
         Assert.That(id, Is.EqualTo(1));
+        id = _accountRepository.SaveAsync(account).Result;
+        Assert.That(id, Is.EqualTo(2));
     }
     
     private async Task ResetDatabaseAsync()
