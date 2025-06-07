0117a16697cccdb89f8b16d7722c7fc44f1f6b11
Alexander Friedrich <a.fr@hotmail.de>
2025-06-07 19:43:40 +0200

    Fix Dependencies für die Tests

--- backend/backend.tests/AccountComponent/Logic/TestAccountHandler.cs
+++ backend/backend.tests/AccountComponent/Logic/TestAccountHandler.cs
@@ -1,10 +1,9 @@
 ﻿using System.Text;
 using System.Text.Json;
-using backend.AccountCompoment.Common.DTOs;
-using backend.AccountCompoment.Dataaccess.Api.Entity;
-using backend.AccountCompoment.Dataaccess.Api.Repo;
-using backend.AccountCompoment.Logic.Api;
-using backend.AccountCompoment.Logic.Impl;
+using backend.AccountComponent.Common.Api.DTOs;
+using backend.AccountComponent.Dataaccess.Api.Repo;
+using backend.AccountComponent.Logic.Api;
+using backend.AccountComponent.Logic.Impl;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Http.HttpResults;
 using Moq;
