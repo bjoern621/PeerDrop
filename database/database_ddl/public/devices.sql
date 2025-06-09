bfdcf6e92c36bd5968d0746d7cf7aa10fdfed346
Alexander Friedrich <a.fr@hotmail.de>
2025-06-09 12:37:36 +0200

    Fix SQL Syntax Fremdschlüsselbeziehung

--- database/database_ddl/public/devices.sql
+++ database/database_ddl/public/devices.sql
@@ -2,7 +2,7 @@ create table devices
 (
     uuid UUID NOT NULL CONSTRAINT devices_pk PRIMARY KEY,
     display_name TEXT NOT NULL,
-    account_id INTEGER NOT NULL CONSTRAINT users_fk FOREIGN KEY
+    account_id INTEGER NOT NULL CONSTRAINT users_fk FOREIGN KEY REFERENCES users(id) ON DELETE CASCADE
 );
 
 alter table devices
