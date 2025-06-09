1dfa6fb3e1c7b035a7ea8f934ce78820f25caf46
Alexander Friedrich <a.fr@hotmail.de>
2025-06-09 03:02:48 +0200

    SQL Syntax Fehler

--- database/database_ddl/public/devices.sql
+++ database/database_ddl/public/devices.sql
@@ -2,7 +2,7 @@ create table devices
 (
     uuid UUID NOT NULL CONSTRAINT devices_pk PRIMARY KEY,
     display_name TEXT NOT NULL,
-    account_id INTEGER NOT NULL CONSTRAINT users_fk FOREIGN KEY,
+    account_id INTEGER NOT NULL CONSTRAINT users_fk FOREIGN KEY
 );
 
 alter table devices
