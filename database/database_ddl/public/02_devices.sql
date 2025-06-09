c3194c4927e1147d7d66bd619e9d34c6797bd818
NovaCaller <dev@novacaller.com>
2025-06-09 11:36:38 +0200

    fixed ddl?

--- database/database_ddl/public/02_devices.sql
+++ database/database_ddl/public/02_devices.sql
@@ -2,7 +2,8 @@ create table devices
 (
     uuid UUID NOT NULL CONSTRAINT devices_pk PRIMARY KEY,
     display_name TEXT NOT NULL,
-    account_id INTEGER NOT NULL CONSTRAINT users_fk FOREIGN KEY
+    account_id INTEGER NOT NULL,
+    CONSTRAINT users_fk FOREIGN KEY (account_id) REFERENCES users(id)
 );
 
 alter table devices
