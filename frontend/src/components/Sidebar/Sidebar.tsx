46b1132f7d4d725287e99fc3e2f9a8a083c29f95
Tim <wvz241@haw-hamburg.de>
2025-06-25 16:38:23 +0200

    Sidebar is collapsed by default

--- frontend/src/components/Sidebar/Sidebar.tsx
+++ frontend/src/components/Sidebar/Sidebar.tsx
@@ -9,7 +9,7 @@ import { StatusResponse } from "../dtos/StatusResponse";
 import { assert } from "../../util/Assert";
 
 export const Sidebar = () => {
-    const [isCollapsed, setIsCollapsed] = useState(false);
+    const [isCollapsed, setIsCollapsed] = useState(true);
     const [showLogin, setShowLogin] = useState(false);
     const [loggedIn, setLoggedIn] = useState(false);
 
