38f65cc74833cf72b1e8e7402da603c5391e55b5
Björn <41452212+bjoern621@users.noreply.github.com>
2025-09-27 13:55:53 +0200

    Fix Confirm Toast Style (introduced with 8ef23ecc8fc9c6fa4eeecc84a013e6d541b33b21)

--- frontend/src/components/Layout/Layout.tsx
+++ frontend/src/components/Layout/Layout.tsx
@@ -2,7 +2,6 @@ import { Outlet } from "react-router";
 import { Sidebar } from "../Sidebar/Sidebar";
 import css from "./Layout.module.scss";
 import { ToastContainer } from "react-toastify/unstyled";
-import "react-toastify/dist/ReactToastify.css";
 
 export const Layout = () => {
     return (
