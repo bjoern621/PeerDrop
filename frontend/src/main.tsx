4b2c366e8d4eda7d9664ffc0389dffa2d957de33
Björn <41452212+bjoern621@users.noreply.github.com>
2025-10-04 14:17:21 +0200

    wip new landing page

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -7,14 +7,16 @@ import LandingPageOld from "./components/LandingPageOld/LandingPageOld.tsx";
 import { Layout } from "./components/Layout/Layout.tsx";
 import { DataSharingPage } from "./components/DataSharingPage/DataSharingPage.tsx";
 import { ConnectionProvider } from "./context/ConnectionProvider.tsx";
+import Landing from "./components/Landing/Landing.tsx";
 
 createRoot(document.getElementById("root")!).render(
     <StrictMode>
         <BrowserRouter>
             <ConnectionProvider>
                 <Routes>
+                    <Route path="/" element={<Landing />} />
                     <Route element={<Layout />}>
-                        <Route path="/" element={<LandingPageOld />} />
+                        <Route path="/old" element={<LandingPageOld />} />
                         <Route path="/share" element={<DataSharingPage />} />
                         <Route path="*" element={<PageNotFound />} />
                     </Route>
