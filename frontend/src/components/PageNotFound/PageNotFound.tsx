931e6d0c7e508fa83369d09b0f2337cade8a155e
Björn <41452212+bjoern621@users.noreply.github.com>
2025-10-17 19:32:27 +0200

    404 angepasst

--- frontend/src/components/PageNotFound/PageNotFound.tsx
+++ frontend/src/components/PageNotFound/PageNotFound.tsx
@@ -1,12 +1,25 @@
 import css from "./PageNotFound.module.scss";
+import Link from "../Button/Link";
 
 export function PageNotFound() {
     return (
-        <div>
-            <h1 className={css.heading}>404 - Page Not Found</h1>
-            <p className={css.paragraph}>
-                Sorry, the page you are looking for does not exist.
-            </p>
+        <div className={css.container}>
+            <div className={css.content}>
+                <div className={css.errorCode}>404</div>
+                <h1 className={css.heading}>
+                    Seite <span className={css.highlight}>nicht gefunden</span>
+                </h1>
+                <p className={css.paragraph}>
+                    Die von dir gesuchte Seite existiert leider nicht.
+                    Vielleicht hast du dich vertippt oder die Seite wurde
+                    verschoben.
+                </p>
+                <div className={css.actions}>
+                    <Link to="/" className={css.homeButton}>
+                        Zurück zur Startseite
+                    </Link>
+                </div>
+            </div>
         </div>
     );
 }
