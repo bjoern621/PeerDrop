ec135de1485ca8d75029d6ffce27ace5480e54f3
Björn <41452212+bjoern621@users.noreply.github.com>
2025-10-17 21:26:48 +0200

    Footer flex instead of grid

--- frontend/src/components/Footer/Footer.tsx
+++ frontend/src/components/Footer/Footer.tsx
@@ -7,49 +7,57 @@ import ThemeToggle from "../ThemeToggle/ThemeToggle";
 export default function Footer() {
     return (
         <footer className={css.footer}>
-            <Logo aria-hidden />
-            <div className={css.heading}>Weiterführende Links</div>
-            <div className={css.heading}>Information</div>
-            <div className={css.heading}>Auch hier</div>
-            <div className={css.version}>
-                Version: <br />
-                {import.meta.env.VITE_APP_VERSION}
+            <div className={css.column}>
+                <Logo aria-hidden />
+                <div className={css.version}>
+                    Version: <br />
+                    {import.meta.env.VITE_APP_VERSION}
+                </div>
             </div>
-            <nav>
-                <ul>
-                    <li>
-                        <Anchor to="/inside" underline="none">
-                            Inside PeerDrop
-                        </Anchor>
-                    </li>
-                    <li>Preise</li>
-                    <li>Entstehung</li>
-                </ul>
-            </nav>
-            <nav>
-                <ul>
-                    <li>
-                        <ThemeToggle />
-                    </li>
-                    <li>Impressum</li>
-                    <li>Datenschutz</li>
-                    <li>Cookies verwalten</li>
-                    <li>Nutzungsbedingungen</li>
-                </ul>
-            </nav>
-            <nav>
-                <ul className={css.socials}>
-                    <li>
-                        <Anchor
-                            to="https://github.com/bjoern621/PeerDrop"
-                            underline="none"
-                            className={css.iconLink}
-                        >
-                            <GitHubIcon aria-hidden /> GitHub
-                        </Anchor>
-                    </li>
-                </ul>
-            </nav>
+            <div className={css.column}>
+                <div className={css.heading}>Weiterführende Links</div>
+                <nav>
+                    <ul>
+                        <li>
+                            <Anchor to="/inside" underline="none">
+                                Inside PeerDrop
+                            </Anchor>
+                        </li>
+                        <li>Preise</li>
+                        <li>Entstehung</li>
+                    </ul>
+                </nav>
+            </div>
+            <div className={css.column}>
+                <div className={css.heading}>Information</div>
+                <nav>
+                    <ul>
+                        <li>
+                            <ThemeToggle />
+                        </li>
+                        <li>Impressum</li>
+                        <li>Datenschutz</li>
+                        <li>Cookies verwalten</li>
+                        <li>Nutzungsbedingungen</li>
+                    </ul>
+                </nav>
+            </div>
+            <div className={css.column}>
+                <div className={css.heading}>Auch hier</div>
+                <nav>
+                    <ul className={css.socials}>
+                        <li>
+                            <Anchor
+                                to="https://github.com/bjoern621/PeerDrop"
+                                underline="none"
+                                className={css.iconLink}
+                            >
+                                <GitHubIcon aria-hidden /> GitHub
+                            </Anchor>
+                        </li>
+                    </ul>
+                </nav>
+            </div>
         </footer>
     );
 }
