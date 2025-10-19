e938ae5c069d399f759475944b58d5461858fc17
Björn <41452212+bjoern621@users.noreply.github.com>
2025-10-19 20:38:33 +0200

    Exported OwnToken Component

--- frontend/src/components/Connection/Connection.tsx
+++ frontend/src/components/Connection/Connection.tsx
@@ -1,10 +1,6 @@
 import css from "./Connection.module.scss";
-import ProfileIcon from "../../assets/icons8-name-tag.svg?react";
 import ConnectIcon from "../../assets/icons8-computers-connecting.svg?react";
 import Button from "../Button/Button";
-import GroupIcon from "../../assets/icons8-group.svg?react";
-import CopyIcon from "../../assets/icons8-copy.svg?react";
-import CopyLinkIcon from "../../assets/icons8-copy-link.svg?react";
 import TokenInput from "./TokenInput/TokenInput";
 import { useCallback, useEffect, useRef, useState } from "react";
 import { useWebSocketService } from "../../context/connection/WebSocketContext";
@@ -15,18 +11,15 @@ import { HEARTBEAT_INTERVAL_MS } from "../../util/Constants";
 import { assert } from "../../util/Assert";
 import { toast } from "react-toastify/unstyled";
 import "react-toastify/dist/ReactToastify.css";
-import errorAsValue from "../../util/ErrorAsValue";
 import { WaitingDialog } from "../Popups/WaitingDialog";
 import { AwaitConnectionDialog } from "../Popups/AwaitConnectionDialog";
 import ConfirmConnectToast from "../ConfirmConnectToast/ConfirmConnectToast";
+import OwnToken from "./OwnToken/OwnToken";
 
 export default function Connection() {
     const websocket = useWebSocketService();
     const peerConnectionManager = usePeerConnectionManager();
 
-    const [clientToken, setClientToken] = useState<string | undefined>(
-        undefined
-    );
     const [remoteToken, setRemoteToken] = useState<string>("");
     const [waitingForResponse, setWaitingForResponse] =
         useState<boolean>(false);
@@ -89,23 +82,6 @@ export default function Connection() {
     const dismissAllToasts = () => toast.dismiss();
 
     useEffect(() => {
-        assert(websocket, "WebSocketService is not initialized.");
-
-        const token = websocket.getLocalClientToken();
-        let checkToken: number | undefined = undefined;
-        if (token) {
-            setClientToken(token);
-        } else {
-            // If not available immediately, set up polling
-            checkToken = setInterval(() => {
-                const token = websocket.getLocalClientToken();
-                if (token) {
-                    setClientToken(token);
-                    clearInterval(checkToken);
-                }
-            }, 500);
-        }
-
         peerConnectionManager.setOnConnectionResponseReceivedCallback(
             (accepted: boolean) => {
                 if (accepted) {
@@ -171,76 +147,6 @@ export default function Connection() {
         declineConnection,
     ]);
 
-    const copyToken = async () => {
-        if (!clientToken) {
-            return;
-        }
-
-        const [, err] = await errorAsValue(
-            navigator.clipboard.writeText(clientToken)
-        );
-
-        if (err) {
-            console.error("Failed to copy token:", err);
-            return;
-        }
-
-        toast.success("Token in die Zwischenablage kopiert!");
-
-        // navigator.clipboard
-        //     .writeText(clientToken)
-        //     .then(() => {
-        //         toast.success("Token in die Zwischenablage kopiert!", {
-        //             toastId: "token-copied-toast",
-        //             updateId: "token-copied-toast",
-        //         });
-        //     })
-        //     .catch(err => {
-        //         console.error("Failed to copy token:", err);
-        //     });
-    };
-
-    const copyTokenLink = async () => {
-        if (!clientToken) {
-            return;
-        }
-
-        const [, err] = await errorAsValue(
-            navigator.clipboard.writeText(
-                `${import.meta.env.VITE_FRONTEND_DOMAIN}/connect/${clientToken}`
-            )
-        );
-
-        if (err) {
-            console.error("Failed to copy token:", err);
-            return;
-        }
-
-        toast.success("Token-Link in die Zwischenablage kopiert!");
-    };
-
-    const openGroupRoom = () => {
-        toast.info(
-            <ConfirmConnectToast
-                requestingPeerToken={"12345"}
-                onAccept={() => {
-                    dismissAllToasts();
-                }}
-                onReject={() => {}}
-                toastId={"12345"}
-            />,
-            {
-                closeOnClick: false,
-                autoClose: false,
-                hideProgressBar: false,
-                progress: 1,
-                closeButton: false,
-                className: "confirm-connection-toast-style", // Set in toast-styles.scss
-                toastId: "12345",
-            }
-        );
-    };
-
     const connectToPeer = () => {
         const successfullySent =
             peerConnectionManager.requestConnectionToRemotePeer(remoteToken);
@@ -268,51 +174,7 @@ export default function Connection() {
 
     return (
         <div className={css.container}>
-            <div className={css.ownTokenContainer}>
-                <h2 className={css.heading}>
-                    <ProfileIcon />
-                    Dein Token
-                </h2>
-
-                <div className={css.tokenBox}>
-                    <div className={css.token}>{clientToken ?? "_____"}</div>
-                    <p className={css.mutedText}>
-                        Teile diesen Token mit anderen
-                    </p>
-                </div>
-
-                <div>
-                    <Button
-                        variant={"outline"}
-                        color_scheme={"neutral"}
-                        className={css.openGroupRoomButton}
-                        onClick={() => openGroupRoom()}
-                    >
-                        <GroupIcon />
-                        Gruppenraum öffnen
-                    </Button>
-                    <div className={css.copyButtons}>
-                        <Button
-                            variant={"outline"}
-                            color_scheme={"neutral"}
-                            disabled={!clientToken}
-                            onClick={() => void copyToken()}
-                        >
-                            <CopyIcon />
-                            Token kopieren
-                        </Button>
-                        <Button
-                            variant={"outline"}
-                            color_scheme={"neutral"}
-                            disabled={!clientToken}
-                            onClick={() => void copyTokenLink()}
-                        >
-                            <CopyLinkIcon />
-                            Token als Link kopieren
-                        </Button>
-                    </div>
-                </div>
-            </div>
+            <OwnToken />
 
             <div className={css.connectToPeerContainer}>
                 <h2 className={css.heading}>
---
+++ frontend/src/components/Connection/OwnToken/OwnToken.tsx
@@ -0,0 +1,127 @@
+import { toast } from "react-toastify/unstyled";
+import css from "./OwnToken.module.scss";
+import errorAsValue from "../../../util/ErrorAsValue";
+import ProfileIcon from "../../../assets/icons8-name-tag.svg?react";
+import GroupIcon from "../../../assets/icons8-group.svg?react";
+import CopyIcon from "../../../assets/icons8-copy.svg?react";
+import CopyLinkIcon from "../../../assets/icons8-copy-link.svg?react";
+import { useEffect, useState } from "react";
+import { assert } from "../../../util/Assert";
+import { useWebSocketService } from "../../../context/connection/WebSocketContext";
+import Button from "../../Button/Button";
+
+export default function OwnToken() {
+    const websocket = useWebSocketService();
+
+    const [clientToken, setClientToken] = useState<string | undefined>(
+        undefined
+    );
+
+    useEffect(() => {
+        assert(websocket, "WebSocketService is not initialized.");
+
+        const token = websocket.getLocalClientToken();
+        let checkToken: number | undefined = undefined;
+        if (token) {
+            setClientToken(token);
+        } else {
+            // If not available immediately, set up polling
+            checkToken = setInterval(() => {
+                const token = websocket.getLocalClientToken();
+                if (token) {
+                    setClientToken(token);
+                    clearInterval(checkToken);
+                }
+            }, 500);
+        }
+    }, [websocket]);
+
+    const copyToken = async () => {
+        if (!clientToken) {
+            return;
+        }
+
+        const [, err] = await errorAsValue(
+            navigator.clipboard.writeText(clientToken)
+        );
+
+        if (err) {
+            console.error("Failed to copy token:", err);
+            return;
+        }
+
+        toast.success("Token in die Zwischenablage kopiert!", {
+            toastId: "token-copied-toast",
+            updateId: "token-copied-toast",
+        });
+    };
+
+    const copyTokenLink = async () => {
+        if (!clientToken) {
+            return;
+        }
+
+        const [, err] = await errorAsValue(
+            navigator.clipboard.writeText(
+                `${import.meta.env.VITE_FRONTEND_DOMAIN}/connect/${clientToken}`
+            )
+        );
+
+        if (err) {
+            console.error("Failed to copy token as link:", err);
+            return;
+        }
+
+        toast.success("Token-Link in die Zwischenablage kopiert!", {
+            toastId: "token-link-copied-toast",
+            updateId: "token-link-copied-toast",
+        });
+    };
+
+    const openGroupRoom = () => {};
+    return (
+        <div className={css.ownTokenContainer}>
+            <h2 className={css.heading}>
+                <ProfileIcon />
+                Dein Token
+            </h2>
+
+            <div className={css.tokenBox}>
+                <div className={css.token}>{clientToken ?? "_____"}</div>
+                <p className={css.mutedText}>Teile diesen Token mit anderen</p>
+            </div>
+
+            <div>
+                <Button
+                    variant={"outline"}
+                    color_scheme={"neutral"}
+                    className={css.openGroupRoomButton}
+                    onClick={() => openGroupRoom()}
+                >
+                    <GroupIcon />
+                    Gruppenraum öffnen
+                </Button>
+                <div className={css.copyButtons}>
+                    <Button
+                        variant={"outline"}
+                        color_scheme={"neutral"}
+                        disabled={!clientToken}
+                        onClick={() => void copyToken()}
+                    >
+                        <CopyIcon />
+                        Token kopieren
+                    </Button>
+                    <Button
+                        variant={"outline"}
+                        color_scheme={"neutral"}
+                        disabled={!clientToken}
+                        onClick={() => void copyTokenLink()}
+                    >
+                        <CopyLinkIcon />
+                        Token als Link kopieren
+                    </Button>
+                </div>
+            </div>
+        </div>
+    );
+}
