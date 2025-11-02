d4dcf42738d7cb8001e55e760ca558d435568afa
Björn <41452212+bjoern621@users.noreply.github.com>
2025-11-02 23:07:04 +0100

    Changed form validation mode to "onChange" just like Login

--- frontend/src/components/Auth/RegisterForm.tsx
+++ frontend/src/components/Auth/RegisterForm.tsx
@@ -24,7 +24,7 @@ export default function RegisterForm({
         formState: { errors, dirtyFields, touchedFields, isSubmitted },
     } = useForm<RegisterFormFields>({
         resolver: zodResolver(registerSchema),
-        mode: "onBlur",
+        mode: "onChange",
     });
 
     const onSubmitForm = (data: RegisterFormFields) => {
