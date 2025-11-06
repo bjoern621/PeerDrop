ac6c63c0646d0f85f3a1376583a95feee34b020e
Björn <41452212+bjoern621@users.noreply.github.com>
2025-11-06 13:58:30 +0100

    Added device heartbeat functionality to Landing component for improved device status management.

--- frontend/src/components/Landing/Landing.tsx
+++ frontend/src/components/Landing/Landing.tsx
@@ -1,3 +1,5 @@
+import { useDeviceHeartbeat } from "../../hooks/useDeviceHeartbeat";
+import { DeviceStatus } from "../../types/device/DeviceStatus";
 import CallToAction from "./CallToAction/CallToAction";
 import Hero from "./Hero/Hero";
 import css from "./Landing.module.scss";
@@ -6,6 +8,8 @@ import UnderTheHood from "./UnderTheHood/UnderTheHood";
 import Why from "./Why/Why";
 
 export default function Landing() {
+    useDeviceHeartbeat({ status: DeviceStatus.ONLINE });
+
     return (
         <div className={css.container}>
             <Hero />
